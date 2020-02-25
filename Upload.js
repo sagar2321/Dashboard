ffe7a5cffacbd5c63fa7fcf1a1dd905009602c54
Sagar <sagarks.2321@gmail.com>
2020-02-25 14:31:37 +0530

    Add files via upload

--- Upload.js
+++ Upload.js
@@ -1,62 +1,97 @@
 import React, {Component} from 'react';
-
-
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
 import Grid from "@material-ui/core/Grid";
-import Fab from '@material-ui/core/Fab';
+import Typography from '@material-ui/core/Typography';
 
+import './Upload.css';
+
+// function handleClick(event) {
+//   event.preventDefault();
+//   console.info('You clicked a breadcrumb.');
+// }
 
  class Upload extends Component{
 
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null
+      }
+   
+  }
+
+
+  onChangeHandler=event=>{
+
+   this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+
+})
+  }
+
     render(){
-
-        const navbarItem={
-            marginLeft:"40%"
-          }
-          const icon={
-            color:"#ff0000"
-          }
-          const style = {
-            minWidth: 300,
-            minHeight:75
-         }
-         const button2={
-          marginTop:"10%"
-          
-        }
-        const notification={
-          marginRight:"3%"
-        }
-        const verticalTab={
-      
-          marginTop:"10%"
-        }
     
+    return (
+      <div className="Container">
+        <div class= 'Row'>
+          <div class='offset-md-3 col-md-6'>
+            <div class='form-group files'>
+              <label>Upload your files</label>
+              <input type="file" class="form-control" multiple="" name="file" onChange={this.onChangeHandler}></input>
+            </div>
+            <button type="button" class="btn btn-success btn-block">Upload</button>
 
-    return(
-        <div>
-            
-            <Grid container spacing={1}>
-             <Grid item xs={12} sm={3}>
-                <div >
-                <Fab  style={style} variant="extended" color="primary"  >Scan OCTSS</Fab>
-                </div>
-                <div style={button2}>
-                <Fab style={style} variant="extended" color="primary">OCT Layer Segmentation</Fab>
-                </div>
-                </Grid>
 
-                <Grid container spacing={1}>
-                    <Grid item xs={12} sm={3}>
-                        <div>
-                            <h1>term</h1>
-                        </div>
-                    </Grid>
-                </Grid>
-</Grid>
+          </div>
+
         </div>
+      
+      </div>
     );
         }
 
 }
 
+
+
+
+
+          {/* <Grid container
+  direction="column"
+  justify="flex-end"
+  alignItems="stretch">
+
+          
+        
+<Breadcrumbs aria-label="breadcrumb">
+              <Link color="inherit" href="/" onClick={handleClick}>
+                <Typography color="textSecondary">Material-UI</Typography>
+              </Link>
+              <Link
+                color="inherit"
+                href="/getting-started/installation/"
+                onClick={handleClick}
+              >
+                Core
+              </Link>
+              <Link
+                color="textPrimary"
+                href="/components/breadcrumbs/"
+                onClick={handleClick}
+                aria-current="page"
+              >
+                Breadcrumb
+              </Link>
+            </Breadcrumbs>
+            
+          </Grid>
+          <Grid item xs={12} sm={6}
+          >
+            <Typography color="textPrimary"> Span </Typography>
+          
+          </Grid>
+       */}
+        
 export default Upload;
